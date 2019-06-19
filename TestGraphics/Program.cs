461bf2dd308dafd233115bbd07be712b86477198
Mohammed <tuniis@live.com>
2019-06-18 22:34:00 -0500

    Misc refactors

--- TestGraphics/Program.cs
+++ TestGraphics/Program.cs
@@ -1,43 +1,20 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-
-namespace TestGraphics
+﻿namespace TestGraphics
 {
+    using System;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
+    using System.IO;
+    
     class Program
     {
-        private static string _output = @"C:\Users\tunii\source\repos\TestApplications\TestGraphics\TestGraphics\Output\";
-
         static void Main(string[] args)
         {
             // Create string to draw.
             var drawString = "Sample Text";
             var filePath = @"C:\Users\tunii\source\repos\TestApplications\TestGraphics\TestGraphics\Images\TestImage.jpg";
 
-            using var bitmap = new Bitmap(filePath);
-            using var graphics = Graphics.FromImage(bitmap);
-
-            Brush brush = new SolidBrush(Color.Red);
-
-            using var font = new Font("Arial", 90, FontStyle.Italic, GraphicsUnit.Pixel);
-
-            var textSize = graphics.MeasureString(drawString, font);
-            var position = new Point(bitmap.Width - ((int)textSize.Width + 10), bitmap.Height - ((int)textSize.Height + 10));
-
-            graphics.DrawString(drawString, font, brush, position);
-
-            var v = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)));
-            var w = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
-            var x = Directory.GetParent(@"./")?.Parent?.FullName ?? "Unknown";
-            var y = Path.GetFullPath(@"./");
-            var z = AppDomain.CurrentDomain.BaseDirectory;
-
-            var outputFilenameWithPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.FullName, "Output", $"{Guid.NewGuid().ToString()}.jpg");
-
-            // using var mStream = new MemoryStream();
-            bitmap.Save(outputFilenameWithPath, ImageFormat.Jpeg);
+            AddText(filePath, drawString, Color.Teal, 120, 50, 50);
 
             // bitmap.Save(mStream, ImageFormat.Png);
 
@@ -45,6 +22,28 @@ namespace TestGraphics
             // return File(mStream.ToArray(), "image/png", file);
 
             Console.WriteLine($"\nPress any key to exit.");
+        }
+
+        static void AddText(string imagePath, string text, Color color, int fontSize = 100, int positionX = 0, int positionY = 0)
+        {
+            using var bitmap = new Bitmap(imagePath);
+            using var graphics = Graphics.FromImage(bitmap);
+
+            Brush brush = new SolidBrush(color);
+
+            using var font = new Font("Arial", fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            /*  var textSize = graphics.MeasureString(text, font);
+                var position = new Point(bitmap.Width - ((int)textSize.Width + relativePositionX), bitmap.Height - ((int)textSize.Height + relativePositionY)); */
+
+            var position = new Point(positionX, positionY);
+
+            graphics.DrawString(text, font, brush, position);
+
+            var outputFilenameWithPath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), "Output", $"{Guid.NewGuid().ToString()}.jpg");
+
+            // using var mStream = new MemoryStream();
+            bitmap.Save(outputFilenameWithPath, ImageFormat.Jpeg);
         }
 
         static void ResizeImage(string filePath, int width, int height)
